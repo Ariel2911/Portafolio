2211aff206c39fc98f864c960a4a47fe760fb49c
Ariel Barrios <ariel29111978@hotmail.com>
2022-05-19 14:49:13 -0300

    Crea la carpeta docs y dentro de está coloca los archivos para producción

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,28 @@
-"use strict";var btnMenu=document.getElementById("btn-menu"),btnMenuBar=document.getElementById("btn-menu-bar"),navBar=document.getElementById("nav-bar"),navBarLink=document.querySelectorAll(".nav-bar__link");btnMenu.addEventListener("click",(function(){btnMenuBar.classList.toggle("btn-menu__bar--active"),navBar.classList.toggle("nav-bar--active")})),navBarLink.forEach((function(e){e.addEventListener("click",(function(){btnMenuBar.classList.toggle("btn-menu__bar--active"),navBar.classList.toggle("nav-bar--active")}))}));var btnSeeMore=document.getElementById("btnSeeMore"),seeMore=document.querySelector(".more-technologies").classList;btnSeeMore.addEventListener("click",(function(){seeMore.toggle("more-technologies--active");var e=Object.values(seeMore).includes("more-technologies--active");btnSeeMore.textContent=e?"ver menos":"ver más"}));
+const btnMenu = document.getElementById('btn-menu');
+const btnMenuBar = document.getElementById('btn-menu-bar');
+const navBar = document.getElementById('nav-bar');
+const navBarLink = document.querySelectorAll('.nav-bar__link');
+
+btnMenu.addEventListener('click', () => {
+  btnMenuBar.classList.toggle('btn-menu__bar--active');
+  navBar.classList.toggle('nav-bar--active');
+});
+
+navBarLink.forEach((item) => {
+  item.addEventListener('click', () => {
+    btnMenuBar.classList.toggle('btn-menu__bar--active');
+    navBar.classList.toggle('nav-bar--active');
+  });
+});
+
+const btnSeeMore = document.getElementById('btnSeeMore');
+const seeMore = document.querySelector('.more-technologies').classList;
+
+btnSeeMore.addEventListener('click', () => {
+  seeMore.toggle('more-technologies--active');
+  const active = Object.values(seeMore).includes('more-technologies--active');
+
+  btnSeeMore.textContent = active ? 'ver menos' : 'ver más';
+});
+
+// console.log('hola');
