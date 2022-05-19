7b873f8ff4c869fb8f8324a9084f153e467546d2
Ariel <63480084+Ariel2911@users.noreply.github.com>
2022-05-19 11:43:48 -0300

    Delete gulpfile.babel.js directory

--- gulpfile.babel.js/purgecssPipe.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { src, dest } = require('gulp');
-const purgecss = require('gulp-purgecss');
-
-const purgecssPipe = () => src('public/styles/styles.css')
-  .pipe(purgecss({ content: ['public/*.html'] }))
-  .pipe(dest('public/styles'));
-
-module.exports = purgecssPipe;
