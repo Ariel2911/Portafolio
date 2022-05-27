5e04164fd83a95eb1dc435263bf2ba254f2b02aa
Ariel Barrios <ariel29111978@hotmail.com>
2022-05-27 19:06:49 -0300

    Agrega la funcionalidad del formulario y modifica alguno estilos

---
+++ src/js/form.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form');
+const response = document.getElementById('response');
+const loader = document.getElementById('loader');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form[3].disabled = true;
+  response.classList.remove('hidden');
+  message.textContent = 'Enviando...';
+
+  fetch('https://formsubmit.co/ajax/ariel29111978@hotmail.com', {
+    method: 'POST',
+    body: new FormData(e.target),
+  })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then(() => {
+      loader.classList.add('hidden');
+      message.textContent = 'Gracias por tú mensaje';
+      form.reset();
+    })
+    .catch((err) => {
+      loader.classList.add('hidden');
+      message.textContent = err;
+      form.reset();
+    });
+
+  // setTimeout(() => {
+  //   loader.classList.add('hidden');
+  //   message.textContent = 'Gracias por tú mensaje';
+  //   // form.reset();
+  // }, 2000);
+  setTimeout(() => {
+    form[3].disabled = false;
+    loader.classList.remove('hidden');
+    response.classList.add('hidden');
+  }, 3000);
+});
