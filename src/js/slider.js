6ce6f4b24150a808c3a6e90eb208af655f3d6287
Ariel Barrios <ariel29111978@hotmail.com>
2022-06-24 19:30:30 -0300

    Modifica las imagenes del proyecto y hace correcciones en el slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,33 +1,23 @@
 (() => {
-  const sliderContainer = document.getElementById("sliderContainer");
-  const sliderItems = parseInt(sliderContainer.childElementCount); // =2
+  const sliderContainer = document.getElementById('sliderContainer');
+  const sliderItems = parseInt(sliderContainer.childElementCount, 10);
 
-  //asigna el tamaño del slider-container
+  // asigna el tamaño del slider-container
   const width = 100 * sliderItems;
-  sliderContainer.style=`width: ${width}%;`;
+  sliderContainer.style = `width: ${width}%;`;
 
-  //asigna la medida del translate
+  const translate = 100 / sliderItems;
   let translateX = 0;
-  let a=0
-  
 
   setInterval(() => {
-    // console.log(translateX)
-    
-    // a++
-    // sliderContainer.setAttribute("style", `transform:translateX(${translateX}%);width: ${width}%;`);
-    // translateX = translateX -50
-    // if(a >= sliderItems) {
-    //   translateX=0
-    //   a=0
-    // }
-    sliderContainer.setAttribute("style", `transform:translateX(${translateX}%);width: ${width}%;`);
-    translateX = translateX -50
+    sliderContainer.setAttribute(
+      'style',
+      `transform:translateX(${translateX}%);width: ${width}%;`,
+    );
+    translateX -= translate;
 
-
-    if(translateX <= -100) {
-      // console.log('if',translateX)
-      translateX=0
+    if (translateX <= -100) {
+      translateX = 0;
     }
-  }, 4000);  
+  }, 4000);
 })();
